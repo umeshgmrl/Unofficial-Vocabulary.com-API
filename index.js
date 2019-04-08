6e960b9c296de421d5d32e968c937de8a5997875
Bhumesh Domala <umeshgmrl@gmail.com>
2019-04-08 22:55:42 +0530

    add cors support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const { fetchSingleWord, fetchWords } = require("./fetch");
 
+app.use(cors());
 app.get("/word/:word", fetchSingleWord);
 app.get("/words/:text", fetchWords);
 
