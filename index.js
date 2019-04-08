c94dc600ca34daaee4deb12c14d10703e4092e71
Bhumesh Domala <bhumesh@codeft.in>
2019-04-08 19:41:23 +0530

    added express

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-const fetch = require('node-fetch');
-const { parse } = require('node-html-parser');
+const express = require('express')
+const app = express()
+const port = 3000
 
-fetch(`https://www.vocabulary.com/dictionary/definition.ajax?search=assess&lang=en`)
-      .then(res => res.text())
-      .then(data => {
-      	const root = parse(data);
-      	const rawText = root.querySelector('p.short').rawText;
-      	console.log({
-      		success: true,
-      		data: rawText
-      	})
-      })
+app.get('/', (req, res) => res.send('Hello World!'))
 
-
-fetch(`https://www.vocabulary.com/dictionary/autocomplete?search=w`)
-      .then(res => res.text())
-      .then(data => {
-        let div = document.createElement('div');
-        div.innerHTML = data;
-        const words = div.innerText.split('\n').filter(Boolean).map(wordData => {
-          let wordArray = wordData.split(' ');
-          return {
-            name: wordArray.shift(),
-            description: wordArray.join(' ')
-          }
-        });
-        this.setState({
-          words
-        }, () => {
-          div = null;
-        })
-      })
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
