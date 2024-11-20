e82f01911fafc378454a7db1de00c3fac1684419
Bhumesh Domala <umeshgmrl@gmail.com>
2024-11-21 01:59:18 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,20 +1,46 @@
-const fetch = require("node-fetch");
 const { parse } = require("node-html-parser");
-const maxWordLimit = 19;
+const execSync = require('child_process').execSync;
+
+function fetchWithCurl(url) {
+  const curlCommand = `curl '${url}' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+  -H 'cache-control: no-cache' \
+  -H 'pragma: no-cache' \
+  -H 'priority: u=0, i' \
+  -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' \
+  -H 'sec-ch-ua-mobile: ?1' \
+  -H 'sec-ch-ua-platform: "Android"' \
+  -H 'sec-fetch-dest: document' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-site: none' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36'`;
+
+
+  return new Promise((resolve, reject) => {
+    try {
+      const response = execSync(curlCommand);
+      resolve(response.toString());
+    } catch (error) {
+      reject(new Error(`Curl request failed: ${error.message}`));
+    }
+  });
+}
+
 
 exports.fetchSingleWord = (req, res) => {
-  fetch(
-    `https://www.vocabulary.com/dictionary/definition.ajax?search=${req.params.word
-    }&lang=en`
+  fetchWithCurl(
+    `https://www.vocabulary.com/dictionary/definition.ajax?search=${req.params.word}&lang=en`
   )
-    .then(res => res.text())
     .then(data => {
       const root = parse(data);
       let rawText;
       try {
-        rawText = root.querySelector("p.short").rawText;
+        rawText = root.querySelector(".short").rawText;
       } catch (e) {
-        rawText = root.querySelector("h3.definition").rawText;
+        rawText = root.querySelector(".long").rawText;
       }
       res.json({
         success: true,
@@ -24,8 +50,7 @@ exports.fetchSingleWord = (req, res) => {
 };
 
 const fetchWordsBase = (text, pageOffset = 0) => {
-  return fetch(`https://www.vocabulary.com/dictionary/autocomplete?search=${text}&startOffset=${pageOffset}`)
-    .then(res => res.text())
+  return fetchWithCurl(`https://www.vocabulary.com/dictionary/autocomplete?search=${text}&startOffset=${pageOffset}`)
     .then(data => {
       const root = parse(data);
       const result = Array.from(root.querySelectorAll("li")).map(el => {
@@ -48,11 +73,10 @@ exports.fetchWords = async (req, res) => {
 };
 
 exports.fetchWordsFull = (req, res) => {
-  fetch(
+  fetchWithCurl(
     `https://www.vocabulary.com/dictionary/definition.ajax?search=${req.params.text
     }&lang=en`
   )
-    .then(res => res.text())
     .then(data => {
       const root = parse(data);
       const result = Array.from(root.querySelectorAll("ol li")).map((el) => {
